452f8e714c0b4b2df736bc446a398c3eb1a7132c
Kseniia Kuznetsova <ksandrasky@ya.ru>
2025-01-02 22:21:50 +0300

    first commit

---
+++ src/test/java/tests/DigDesTests.java
@@ -0,0 +1,105 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class DigDesTests extends TestBase{
+
+    @Tag("Regression")
+    @DisplayName("О компании: Библиотека технических заданий содержит список документов")
+    @Test
+    void techLibraryContainsDocumentsTest(){
+        step("Открыть главную страницу", () -> {
+            open("");
+        });
+        step("Перейти в раздел О компании->Библиотека технических заданий", () -> {
+            $("#dd-menu").$(byText("О компании")).click();
+            $("li.dropdown.open").$(byText("Библиотека технических заданий")).click();
+        });
+        step("Найти список документов", () -> {
+            $("div#library-list").shouldHave(text("Примеры технических заданий для наших решений"));
+            $("div#library-list").$("div.items").shouldBe(not(empty));
+        });
+    }
+
+    @Tag("Smoke")
+    @Tag("Regression")
+    @DisplayName("О компании: Контакты. Проверка данных офиса в Санкт-Петербурге")
+    @Test
+    void spbOfficeDataCheckingTest(){
+        step("Открыть главную страницу", () -> {
+            open("");
+        });
+        step("Перейти в раздел О компании->Контакты", () -> {
+            $("#dd-menu").$(byText("О компании")).click();
+            $("li.dropdown.open").$(byText("Контакты")).click();
+        });
+        step("Проверить данные офиса в г.Санкт-Петербург", () -> {
+            $("#contacts-offices").shouldHave(text("Санкт-Петербург"));
+            $$("div.item").get(0).$("div.city").shouldHave(text("Санкт-Петербург"));
+            $$("div.item").get(0).$("[itemprop=postalCode]").shouldHave(text("199178"));
+            $$("div.item").get(0).$("[itemprop=streetAddress]").shouldHave(text("наб. реки Смоленки, д. 33"));
+            $$("div.item").get(0).$("[itemprop=openingHours]").shouldHave(text("Пн-Пт 10:00-19:00"));
+        });
+    }
+
+    @Tag("Regression")
+    @DisplayName("О компании: Руководство. Проверка отображения генерального директора")
+    @Test
+    void checkCeoNameTest(){
+        step("Открыть главную страницу", () -> {
+            open("");
+        });
+        step("Перейти в раздел О компании->Руководство", () -> {
+            $("#dd-menu").$(byText("О компании")).click();
+            $("li.dropdown.open").$(byText("Руководство")).click();
+        });
+        step("Кликнуть карточку с ФИО генерального директора", () -> {
+            $("div#management-managers").$(byText("Генеральный директор компании Digital Design")).click();
+        });
+        step("Проверить отображение ФИО генерального директора в модальном окне", () -> {
+            $("div#modal-m-varfolomeev").$("h4.modal-title").shouldHave(text("Антон Владиславович Варфоломеев"), text("Генеральный директор компании Digital Design"));
+        });
+    }
+
+    @Tag("Smoke")
+    @Tag("Regression")
+    @DisplayName("Научная лаборатория: Проверка пунктов выпадающего меню")
+    @Test
+    void checkScienceLabMenuTest(){
+        step("Открыть главную страницу", () -> {
+            open("");
+        });
+        step("Кликнуть раздел Научная лаборатория", () -> {
+            $("#dd-menu").$(byText("Научная лаборатория")).click();
+        });
+        step("Проверить список пунктов", () -> {
+            $("li.dropdown.open").shouldHave(text("О научной лаборатории"),
+                    text("Автоматическая проверка документов"), text("Интеллектуальная обработка корреспонденции"),
+                    text("Прогнозирование водно-энергетических режимов"));
+        });
+    }
+
+    @Tag("Regression")
+    @DisplayName("Карьера: Наша команда. Страница содержит видеообращение генерального директора")
+    @Test
+    void ceoVideoExistsTest(){
+        step("Открыть главную страницу", () -> {
+            open("");
+        });
+        step("Перейти в раздел Карьера->Наша команда", () -> {
+            $("#dd-menu").$(byText("Карьера")).click();
+            $("li.dropdown.open").$(byText("Наша команда")).click();
+        });
+        step("Проверить наличие видеообращения", () -> {
+            $("div#career-new-video").shouldHave(text("Обращение генерального директора")).scrollTo();
+            $("div.adaptive-video").shouldBe(visible);
+        });
+    }
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,51 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+
+    @BeforeAll
+    static void preconditionsForAllTests() {
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.baseUrl = "https://digdes.ru/";
+        Configuration.holdBrowserOpen = false;
+        Configuration.remote = System.getProperty("remoteUrl");
+        Configuration.browser = System.getProperty("browserName", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.pageLoadStrategy = "eager";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void preconditionForEachTest(){
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+        closeWebDriver();
+
+    }
+}
